f2188eb5f5c8557ecaf78e6cbbe9234ce2726ec0
Roberto Micheletti <michelettiroberto7@gmail.com>
2022-07-03 23:32:55 +0200

    get metadata from android 11 and more, for android 10 has a error

--- android/src/main/java/com/mike/metadata/Metadata.java
+++ android/src/main/java/com/mike/metadata/Metadata.java
@@ -1,31 +1,90 @@
 package com.mike.metadata;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 
+import java.io.File;
+import java.util.HashMap;
+
 public class Metadata {
+    Context context;
 
-    public String[] metadata(String pathFile) {
+    public Metadata(Context context) {
+        this.context = context;
+    }
+
+    public HashMap<String, String> metadata(String pathFile) {
         Log.i("Metadata", pathFile);
-        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-        mmr.setDataSource(pathFile);
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(pathFile);
+        } catch (Exception e) {
+            Log.i("Metadata Exception", e.getMessage());
+        }
 
-        String album = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-        String albumArtist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUMARTIST);
-        String artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
-        String author = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_AUTHOR);
-        String date = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DATE);
-        String title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-        String duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        HashMap<String, String> value = new HashMap<>();
+        value.put("album", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM));
+        value.put("albumArtist", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUMARTIST));
+        value.put("artist", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
+        value.put("author", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_AUTHOR));
+        value.put("date", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DATE));
+        value.put("title", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
+        value.put("duration", retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
 
-        return new String[]{
-                album,
-                albumArtist,
-                artist,
-                author,
-                date,
-                title,
-                duration,
+        return value;
+    }
+
+    public HashMap<String, String> cursor(String pathFile) {
+        Log.i("Metadata", pathFile.replace("file:///storage/emulated/0//Music/", ""));
+        String[] projection = {
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DISPLAY_NAME,
         };
+        Cursor cursor = null;
+        try {
+            Log.i("Cursor", "Getting cursor");
+
+            cursor = this.context.getContentResolver().query(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    null
+            );
+
+            Log.i("Cursor", "Obtained");
+        } catch (Exception e) {
+            Log.i("Cursor", "Error");
+            Log.e("Cursor", e.getMessage());
+        }
+
+        HashMap<String, String> value = new HashMap<>();
+
+        Log.i("Cursor", cursor == null ? "I am null" : "Yeah");
+        if (cursor != null) {
+            Log.i("Cursor message", "Cursor is not null");
+            Log.i("Cursor count", "" + cursor.getCount());
+            while (cursor.moveToNext()) {
+                Log.i("Metadata message", "Cursor is not empty");
+                value.put("album", cursor.getString(0));
+                value.put("title", cursor.getString(1));
+                value.put("duration", cursor.getString(2));
+                value.put("date", cursor.getString(3));
+                value.put("artist", cursor.getString(4));
+                value.put("displayName", cursor.getString(5));
+            }
+            cursor.close();
+        }
+        Log.i("Cursor", "Finished");
+
+        return value;
     }
 }
--- android/src/main/java/com/mike/metadata/MetadataPlugin.java
+++ android/src/main/java/com/mike/metadata/MetadataPlugin.java
@@ -1,22 +1,37 @@
 package com.mike.metadata;
 
+import android.os.Build;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import java.util.HashMap;
+
 @CapacitorPlugin(name = "Metadata")
 public class MetadataPlugin extends Plugin {
 
-    private Metadata implementation = new Metadata();
+    private Metadata implementation;
+
+    @Override
+    public void load() {
+        implementation = new Metadata(getContext());
+    }
 
     @PluginMethod
-    public void metadata(PluginCall call) {
+    public void metadata(PluginCall call) throws Exception {
         String pathFile = call.getString("value");
 
         JSObject ret = new JSObject();
-        ret.put("metadata", implementation.metadata(pathFile));
+        HashMap<String, String> value;
+        if (Build.VERSION.SDK_INT >= 30) {
+            value = implementation.metadata(pathFile);
+        } else {
+            value = implementation.cursor(pathFile);
+        }
+        ret.put("metadata",value);
         call.resolve(ret);
     }
 }
